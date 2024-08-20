a403b83ec7edd7ce5f91901c29cabdc6f3645214
Brasco2783 <113712985+Brasco2783@users.noreply.github.com>
2024-08-20 09:38:03 -0700

    Create test_new_project_.py

---
+++ test_new_project_.py
@@ -0,0 +1,39 @@
+import self
+from selenium import webdriver  # This line imports the webdriver module
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from time import sleep
+
+from Tests.Reusable_Login_Orch import login_to_orchestra
+
+
+class TestOrchestra:
+    def test_login(self):
+        browser = login_to_orchestra('Dean75@pretreer.com', "4xlOI4vb_cB0h8kY99sV")
+
+        # Click 'task icon'
+        WebDriverWait(browser, 20).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@class='nav-bar-element navbar-action']"))
+        ).click()
+
+        # Click plus icon button
+        plus_icon = WebDriverWait(browser, 20).until(
+            EC.element_to_be_clickable(
+                (By.XPATH, "//button[@data-v-754d6337='' and @data-v-c62a3f61='']")
+            )
+        )
+        plus_icon.click()
+
+        # Verify 'New Project' text is shown
+        actual_results = (
+            browser.find_element(By.XPATH, "//div[@class='or-input-placeholder' and text()='New Project']")
+            .text)
+        expected_results = 'New Project'
+        assert actual_results == expected_results
+
+
+sleep(15)
