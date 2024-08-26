68ff8534a82c4241d0f77a08ba2f7308bf58b80a
Don Comeaux <113712985+Brasco2783@users.noreply.github.com>
2024-08-26 13:02:12 -0700

    Update test_header_JO.py

--- test_header_JO.py
+++ test_header_JO.py
@@ -6,7 +6,7 @@
 
 
 import self
-from selenium import webdriver  # This line imports the webdriver module
+from selenium import webdriver  
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
